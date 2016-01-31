80dc23b30631bb40e372bcde366522e0b2819ea2
Dan Evron <dan.evron@klarna.com>
2016-01-31 12:25:11 +0200

    Adds timesheet view

--- src/components/timesheet.js
+++ src/components/timesheet.js
@@ -1,19 +1,58 @@
 var React = require('react-native');
 var NavBar = require('rn-navbar');
+var moment = require('moment');
+var Calendar = require('./calendar');
 
 var {
   View,
   StyleSheet,
-  Text
+  Text,
+  TouchableHighlight
 } = React;
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return (
+      {
+        selectedDate: null
+      }
+    );
+  },
   render: function() {
     return (
-      <View>
+      <View style={styles.container}>
         <NavBar title="Current Timesheet" backFunc={()=>{this.props.navigator.pop()}} />
-        <View style={styles.container}>
-          <Text>TODO: timesheet view</Text>
+
+        <View style={styles.calendar}>
+          <Calendar
+            ref="calendar"
+            workdayDates={['2016-01-18']}
+            weekendDates={['2016-01-19']}
+            vacationDates={['2016-01-20']}
+            holidayDates={['2016-01-21']}
+            sicknessDates={['2016-01-22']}
+            armyDates={['2016-01-23']}
+            scrollEnabled={true}
+            showControls={true}
+            titleFormat={'MMMM YYYY'}
+            prevButtonText={'Prev'}
+            nextButtonText={'Next'}
+            onDateSelect={(date) => this.setState({selectedDate: date})}
+            onTouchPrev={() => console.log('Back TOUCH')}
+            onTouchNext={() => console.log('Forward TOUCH')}
+            onSwipePrev={() => console.log('Back SWIPE')}
+            onSwipeNext={() => console.log('Forward SWIPE')}/>
+        </View>
+        <View style={styles.buttons}>
+          <TouchableHighlight style={styles.submitButton} onPress={() => {this.signIn(); }}>
+            <Text>Submit</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.prefillButton} onPress={() => {this.signIn(); }}>
+            <Text>Pre-fill working days</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.extractButton} onPress={() => {this.signIn(); }}>
+            <Text>Extract calendar events</Text>
+          </TouchableHighlight>
         </View>
       </View>
     );
@@ -22,8 +61,30 @@ module.exports = React.createClass({
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
+  },
+  calendar: {
+    flex: 0.5
+  },
+  buttons: {
+    flex: 0.5
+  },
+  submitButton: {
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'green'
+  },
+  prefillButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'yellow'
+  },
+  extractButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'orange'
   }
 });
